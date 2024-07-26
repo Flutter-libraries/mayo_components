73fe7997d043c625345419edbc403c3a09b54020
Miguel <miguel@mayosoftware.es>
2024-07-26 17:21:14 +0200

    Enhancements on read more

--- lib/src/components/text_read_more.dart
+++ lib/src/components/text_read_more.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 const kMaxLines = 5;
 
@@ -32,52 +33,61 @@ class _TextReadMoreState extends State<TextReadMore> {
   bool unread = true;
   @override
   Widget build(BuildContext context) {
-    final span = TextSpan(
-      text: widget.text,
-      style: widget.textStyle,
-    );
-    final tp = TextPainter(text: span, textDirection: TextDirection.ltr)
-      ..layout(maxWidth: MediaQuery.of(context).size.width - 64);
-    final numLines = tp.computeLineMetrics().length;
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Stack(
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final span = TextSpan(
+          text: widget.text,
+          style: widget.textStyle,
+        );
+        final tp = TextPainter(text: span, textDirection: TextDirection.ltr)
+          ..layout(maxWidth: constraints.maxWidth);
+        final numLines = tp.computeLineMetrics().length;
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16),
-              child: Text(
-                widget.text,
-                style: widget.textStyle,
-                maxLines: unread ? widget.maxLines : null,
-                overflow: unread ? TextOverflow.fade : null,
-              ),
+            Stack(
+              children: [
+                IntrinsicHeight(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: Text(
+                      widget.text,
+                      style: widget.textStyle,
+                      maxLines: unread ? widget.maxLines : null,
+                      overflow: unread
+                          ? kIsWeb
+                              ? TextOverflow.ellipsis
+                              : TextOverflow.fade
+                          : null,
+                    ),
+                  ),
+                ),
+                if (unread && numLines > widget.maxLines)
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: widget.clickable
+                        ? GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                unread = false;
+                              });
+                            },
+                            child: Text(
+                              widget.buttonText,
+                              style: widget.buttonStyle,
+                            ),
+                          )
+                        : Text(
+                            widget.buttonText,
+                            style: widget.buttonStyle,
+                          ),
+                  ),
+              ],
             ),
-            if (unread && numLines > widget.maxLines)
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: widget.clickable
-                    ? GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            unread = false;
-                          });
-                        },
-                        child: Text(
-                          widget.buttonText,
-                          style: widget.buttonStyle,
-                        ),
-                      )
-                    : Text(
-                        widget.buttonText,
-                        style: widget.buttonStyle,
-                      ),
-              ),
           ],
-        ),
-      ],
+        );
+      },
     );
   }
 }
